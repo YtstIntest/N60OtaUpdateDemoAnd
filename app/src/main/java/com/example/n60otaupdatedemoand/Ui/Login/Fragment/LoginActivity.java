de6dc5f266c43c17344a7d1cf995355f5e21d544
xialiang <xialiang@intest.cn>
2020-05-11 12:26:40 +0800

    新增登陆界面

---
+++ app/src/main/java/com/example/n60otaupdatedemoand/Ui/Login/Fragment/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.n60otaupdatedemoand.Ui.Login.Fragment;
+
+import android.os.Bundle;
+
+import com.example.library.base.BaseAppActivity;
+
+public class LoginActivity extends BaseAppActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void afterCreate(Bundle savedInstanceState) {
+
+    }
+}
